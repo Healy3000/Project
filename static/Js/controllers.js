58eaab36fb9f3a84ee02f2c92da963c0f714d5bf
Healy3000 <edward.francs.healy@outlook.com>
2017-10-30 19:44:47 +0000

    Verison 1

---
+++ static/Js/controllers.js
@@ -0,0 +1,7 @@
+
+		function SimpleController($scope)
+		{$scope.Memebers = [
+		{name:'Peter Tork', picture: 'London'}, {name:'Micky Dolenz', picture: 'ondon'},
+		{name: 'Davy Jones', picture: ' ' }, { name: 'Michael Nesmith', picture: 'london' }];
+		
+		};
